e2a40f8012d71c48f6b4ee961731b8eaeb069e6d
Ben Roberts <ben@sihnon.net>
2016-10-09 13:47:43 +0100

    Add autorequires for enman_repo on package[enman]

--- lib/puppet/type/enman_repo.rb
+++ lib/puppet/type/enman_repo.rb
@@ -7,5 +7,12 @@ Puppet::Type.newtype(:enman_repo) do
     desc "Name of the Enman Repository"
     isnamevar
   end
+
+  autorequire(:package) do
+    ['enman']
+  end
   
 end
+
+# vim: set ts=2 shiftwidth=2 expandtab :
+
